41966bb9d0dc99ae3c9f5ce1c04d4c5a50952c0a
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-06 12:59:49 +1100

    Data Types

---
+++ DataTypes/script.js
@@ -0,0 +1,59 @@
+console.log(typeof 15)
+// Prediction: number
+// Actual: number
+
+console.log(typeof 5.5)
+// Prediction: number 
+// Actual: number 
+
+console.log(typeof NaN)
+// Prediction: NaN
+// Actual: number
+
+console.log(typeof "hello")
+// Prediction: string
+// Actual: string
+
+console.log(typeof true)
+// Prediction: boolean
+// Actual: boolean
+
+console.log(typeof 1 != 2)
+// Prediction: boolean
+// Actual: true
+
+console.log("hamburger" + "s")
+// Prediction: hamburgers
+// Actual: hamburgers
+
+console.log("hamburgers" - "s")
+// Prediction: NaN
+// Actual: NaN
+
+console.log("1" + "3")
+// Prediction: 13
+// Actual: 13
+
+console.log("1" - "3")
+// Prediction: -2
+// Actual: -2
+
+console.log("johnny" + 5)
+// Prediction: johnny5
+// Actual: johnny5
+
+console.log("johnny" - 5)
+// Prediction: NaN
+// Actual: NaN
+
+console.log(99 * "luftbaloons")
+// Prediction: NaN
+// Actual: NaN
+
+console.log(5 + true)
+// Prediction: 6
+// Actual: 6
+
+console.log(5 + false)
+// Prediction: 5
+// Actual: 5

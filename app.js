82e4dd4abe31f9004580d06d3d6e174fad460d88
Penelope Chen <pen.c828@gmail.com>
2022-12-06 11:57:20 +1100

    Completed JS-Primitives lab

---
+++ app.js
@@ -0,0 +1,68 @@
+// Fortune Teller
+const jobTitle = "Programmer"
+const geoLocation = "Portugal"
+const favSport = "Tennis"
+const sideProject = "building a Flying Car"
+console.log(`You will be a ${jobTitle} in ${geoLocation}, playing ${favSport} while ${sideProject}.`)
+
+// Temperature Converter
+// °C -> °F
+// const celsius = 35
+// const fahrenheit = (celsius * 9 / 5) + 32
+// console.log(`${celsius}°C is ${fahrenheit}°F.`)
+// // °F -> °C
+// const fahrenheit = 68;
+// const celsius = (fahrenheit - 32) * 5 / 9;
+// console.log(`${fahrenheit}°F is ${celsius}°C.`);
+
+// Age Calculator 
+const birthYear = 1998
+const futureYear = 3000
+console.log(`I will be either ${(futureYear - birthYear)} or ${(futureYear - birthYear + 1)} in ${futureYear}.`);
+
+// Data Types 
+console.log(typeof 15) 
+// Prediction: number 
+// Actual: number
+console.log(typeof 5.5) 
+// Prediction: number 
+// Actual: number
+console.log(typeof NaN)
+// Prediction: number
+// Actual: number
+console.log(typeof "hello")
+// Prediction: string
+// Actual: string
+console.log(typeof true)
+// Prediction: boolean
+// Actual: boolean
+console.log(typeof 1 != 2)
+// Prediction: boolean 
+// Actual: true
+console.log("hamburger" + "s")
+// Prediction: hamburgers 
+// Actual: hamburgers
+console.log("hamburgers" - "s")
+// Prediction: hamburger
+// Actual: NaN
+console.log("1" + "3")
+// Prediction: 13 
+// Actual: 13
+console.log("1" - "3")
+// Prediction: undefined
+// Actual: -2
+console.log("johnny" + 5)
+// Prediction: johnny5 
+// Actual: johnny5
+console.log("johnny" - 5)
+// Prediction: undefined
+// Actual: NaN
+console.log(99 * "luftbaloons")
+// Prediction: undefined 
+// Actual: NaN
+console.log(5 + true)
+// Prediction: undefined 
+// Actual: 6
+console.log(5 + false)
+// Prediction: undefined 
+// Actual: 5

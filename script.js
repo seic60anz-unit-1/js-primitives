e1f8964b6c040b78b0456668b3c3b9f6c497dd35
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-06 11:42:54 +1100

    Fortune telling is superstitious.

---
+++ script.js
@@ -0,0 +1,6 @@
+//Question 1
+const jobTitle = "Programmer"
+const geoLocation = "Australia"
+const favSport = "football"
+const sideProj = "running a theatre"
+console.log(`You will be a ${jobTitle} in ${geoLocation}, playing ${favSport} while ${sideProj}.`)

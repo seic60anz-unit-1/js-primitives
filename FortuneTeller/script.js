d7ccea1bff3a90edea168c8de4ad3ad86b04d9f2
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-06 11:33:22 +1100

    FortuneTeller work

---
+++ FortuneTeller/script.js
@@ -0,0 +1,9 @@
+
+let jobTitle = prompt('please enter your job title')
+let geolocation = prompt('please enter your geo location')
+let favouriteSport = prompt('please enter your favourite sport')
+let sideProject = prompt('please enter your side project')
+console.log('You will be a ' + jobTitle + ' in ' + geolocation + ', playing ' + favouriteSport + ' while doing ' + sideProject + '.')
+
+
+

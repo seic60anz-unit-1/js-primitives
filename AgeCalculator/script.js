acacf1134a0adb61136b4498d7e121cbe9231a13
JP <jpcodingjourney@JPs-MacBook-Air.local>
2022-12-06 12:43:21 +1100

    AgeCalculator

---
+++ AgeCalculator/script.js
@@ -0,0 +1,6 @@
+let birthYear = prompt('your birth year')
+let futureYear = prompt('say a future year')
+yearA = parseInt(futureYear - birthYear)
+yearB = parseInt((futureYear - birthYear) + 1)
+
+alert('I will be either ' + yearA + ' or ' + yearB + ' in ' + futureYear)

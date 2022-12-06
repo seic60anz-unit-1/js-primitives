64f8f4cd01fb04d159acfb0ccee60ea5395f0675
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-06 11:44:10 +1100

    Temperature is an objective measurment.

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const geoLocation = "Australia"
 const favSport = "football"
 const sideProj = "running a theatre"
 console.log(`You will be a ${jobTitle} in ${geoLocation}, playing ${favSport} while ${sideProj}.`)
+
+//Question 2
+
+function CtoF(temp) {
+    return (temp*9/5+32)
+}
+let celsius = 25
+console.log(`${celsius}°C is ${CtoF(celsius)}°F.`)
+
+function FtoC(temp) {
+    return ((temp-32)*5/9)
+}
+let fahrenheit = 32
+console.log(`${fahrenheit}°F is ${FtoC(fahrenheit)}°C.`)
+
